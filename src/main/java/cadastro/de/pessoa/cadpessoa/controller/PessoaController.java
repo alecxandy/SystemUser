47d3029ab2866148e95ccbc3c11725a0399b6e1c
alexandre gomes <alecxandy@gmail.com>
2022-03-23 04:15:31 -0300

    salvando pessoas

--- src/main/java/cadastro/de/pessoa/cadpessoa/controller/PessoaController.java
+++ src/main/java/cadastro/de/pessoa/cadpessoa/controller/PessoaController.java
@@ -1,10 +1,13 @@
 package cadastro.de.pessoa.cadpessoa.controller;
 
+import cadastro.de.pessoa.cadpessoa.model.Pessoa;
 import cadastro.de.pessoa.cadpessoa.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PessoaController {
@@ -14,8 +17,20 @@ public class PessoaController {
 
     @GetMapping("/pessoas")
     public String index(Model model) {
-        model.addAttribute("listaPessoas",pessoaRepository.findAll());
+        model.addAttribute("listaPessoas", pessoaRepository.findAll());
         return "index";
     }
+
+    @GetMapping("/pessoas/nova")
+    public String novaPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
+        return "form";
+    }
+
+    @PostMapping("/pessoas/salvar")
+    public String pessoaSalvar(@ModelAttribute("pessoa") Pessoa pessoa) {
+        pessoaRepository.save(pessoa);
+        return "redirect:/pessoas";
+    }
+
 
 }
